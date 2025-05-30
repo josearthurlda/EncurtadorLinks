1c72f289cb40043bff0dd70f0f0cb3755eb0ee64
josearthurlda <josearthurlda@gmail.com>
2025-05-30 20:40:58 -0300

    Conexão com banco de dados feita

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        require_once("./config/database/database.php");
+    
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Encurtador</title>
 </head>
 <body>
-    
+
 </body>
 </html>
