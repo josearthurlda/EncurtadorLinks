46788a9c9d19d0a3963c80585a20af97b29cfd6d
josearthurlda <josearthurlda@gmail.com>
2025-05-30 22:10:22 -0300

    Design da pagina inicial feito

--- index.php
+++ index.php
@@ -11,26 +11,47 @@
     <title>Encurtador</title>
 </head>
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-    <form action="./actions/encurtar.php" method="POST">
-        <label for="link">Link para ser encurtado:</label>
-        <input class="form-control" name="link" id="link" type="url" placeholder="seulink.com" required>
-        <button type="submit" class="btn btn-primary">Encurtar</button>
-    </form>
-    <?php
-      $por_pagina = 10;
-      $pagina = $_GET['pagina'] ?? 1;
-      $inicio = ($pagina - 1) * $por_pagina;
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./index.php">Encurtador</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Links</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <main class="d-flex flex-column justify-content-center position-absolute top-50 start-50 translate-middle">
+    <h1>Encurtador de Links</h1>
+    <div class="card">
+      <div class="card-header text-center">
+        Encurtar link
+      </div>
+      <div class="card-body text-center">
+        <form action="./actions/encurtar.php" method="POST">
+            <label for="link">Link para ser encurtado:</label>
+            <input class="form-control mt-2 mb-2" name="link" id="link" type="url" placeholder="seulink.com" required>
+            <button type="submit" class="btn btn-primary">Encurtar</button>
+        </form>
+      </div>
+    </div>
 
-      $sql = "SELECT * FROM links ORDER BY id DESC LIMIT $inicio, $por_pagina"; 
+  </main>
+
+    <?php
+      $sql = "SELECT * FROM links ORDER BY id"; 
       $rows = $con->query($sql);
       if($rows->num_rows > 0){
         while($row = $rows->fetch_assoc()){
           echo '
             <tr class="text-center">
-              <th scope="row">'.$row['id'].'</th>
               <td>'.$row['link_longo'].'</td>
-              <td><a href="./'.$row['link_curto'].'?id='.$row['link_curto'].'">Link Encurtado</a></td>
+              <td><a href="?id='.$row['link_curto'].'">Link Encurtado</a></td>
               <td>
                 <a class="btn btn-danger" href="actions/deletar.php?id='.$row['id'].'">
                   <i class="bi bi-trash"></i>
